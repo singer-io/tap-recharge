694a4a5949ca08b8d73d1525017006f277ef9ea1
Andrey Kabanov <andrey.kabanov@bytecode.io>
2021-09-23 16:30:59 -0700

    removing extraneous keys and related functions from discover.py

--- tap_recharge/discover.py
+++ tap_recharge/discover.py
@@ -1,25 +1,6 @@
 from singer.catalog import Catalog
 from tap_recharge.schema import get_schemas
 
-def _get_key_properties_from_meta(schema_meta: list) -> str:
-    """
-    Gets the table-key-properties from the schema metadata.
-    """
-    return schema_meta[0].get('metadata').get('table-key-properties')
-
-def _get_replication_method_from_meta(schema_meta: list) -> str:
-    """
-    Gets the forced-replication-method from the schema metadata.
-    """
-    return schema_meta[0].get('metadata').get('forced-replication-method')
-
-def _get_replication_key_from_meta(schema_meta: list) -> str:
-    """
-    Gets the valid-replication-keys from the schema metadata.
-    """
-    if _get_replication_method_from_meta(schema_meta) == 'INCREMENTAL':
-        return schema_meta[0].get('metadata').get('valid-replication-keys')[0]
-    return None
 
 def discover():
     """
@@ -35,9 +16,6 @@ def discover():
             'stream': schema_name,
             'tap_stream_id': schema_name,
             'schema': schema,
-            'key_properties': _get_key_properties_from_meta(schema_meta),
-            'replication_method': _get_replication_method_from_meta(schema_meta),
-            'replication_key': _get_replication_key_from_meta(schema_meta),
             'metadata': schema_meta
         }
 
