571e4306cda6ff04d9b66ae12060db5bfbb3a829
harshpatel4crest <harsh.patel4@crestdatasys.com>
2022-06-15 18:22:27 +0530

    updated unittests

--- tests/unittests/test_products_stream_verison.py
+++ tests/unittests/test_products_stream_verison.py
@@ -13,7 +13,6 @@ class Mockresponse:
     def json(self):
         return self.text
 
-@mock.patch('requests.Session.request')
 class TestProductsVerison(unittest.TestCase):
     """
         Test cases to verify that we are calling 'Products' with API Version: '2021-01'
@@ -21,41 +20,29 @@ class TestProductsVerison(unittest.TestCase):
 
     recharge_client = RechargeClient('test_access_token')
 
+    @mock.patch('requests.Session.request', return_value=Mockresponse({'products': [{'key': 'value'}]}))
     def test_products_stream_version(self, mocked_request):
         """
             Test case to verify we are calling stream 'Products' with API Version: '2021-01'
         """
-        # mock request and return value with data key
-        mocked_request.return_value = Mockresponse({'products': [{'key': 'value'}]})
-
-        # Products object
         products = Products(self.recharge_client)
-        # function call
         list(products.get_records())
 
-        # get arguments passed during calling "requests.Session.request"
         args, kwargs = mocked_request.call_args
-        # get 'headers' for API call
         request_headers = kwargs.get('headers')
 
         # verify we using API Version: '2021-01'
         self.assertEqual(request_headers.get('X-Recharge-Version'), '2021-01')
 
+    @mock.patch('requests.Session.request', return_value=Mockresponse({'collections': [{'key': 'value'}]}))
     def test_non_products_stream_version(self, mocked_request):
         """
             Test case to verify we are calling stream other than 'Products' with API Version: '2021-11'
         """
-        # mock request and return value with data key
-        mocked_request.return_value = Mockresponse({'collections': [{'key': 'value'}]})
-
-        # Collections object
         collections = Collections(self.recharge_client)
-        # function call
         list(collections.get_records())
 
-        # get arguments passed during calling "requests.Session.request"
         args, kwargs = mocked_request.call_args
-        # get 'headers' for API call
         request_headers = kwargs.get('headers')
 
         # verify we using API Version: '2021-11'
