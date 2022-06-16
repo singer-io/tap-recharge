9097359f886a83dc41ed2f3249654d15e43d4e07
savan-chovatiya <savan.chovatiya@crestdatasys.com>
2022-06-16 13:33:15 +0530

    Added missing integration test and missing assertions

---
+++ tests/test_recharge_all_fields.py
@@ -0,0 +1,79 @@
+from tap_tester import runner, connections, menagerie
+from base import RechargeBaseTest
+
+
+class RechargeAllFieldsTest(RechargeBaseTest):
+    
+    def name(self):
+        return "tap_tester_recharge_all_fields_test"   
+
+    def test_run(self):
+        """
+        • Verify no unexpected streams were replicated
+        • Verify that more than just the automatic fields are replicated for each stream. 
+        • verify all fields for each stream are replicated
+        """
+        
+        # Streams to verify all fields tests
+        expected_streams = self.expected_streams()
+
+        expected_automatic_fields = self.expected_automatic_fields()
+        
+        # instantiate connection
+        conn_id = connections.ensure_connection(self)
+        
+        # instantiate connection
+        found_catalogs = self.run_and_verify_check_mode(conn_id)
+
+        # table and field selection
+        test_catalogs_all_fields = [catalog for catalog in found_catalogs
+                                    if catalog.get('tap_stream_id') in expected_streams]
+
+        self.perform_and_verify_table_and_field_selection(
+            conn_id, test_catalogs_all_fields)
+
+        # grab metadata after performing table-and-field selection to set expectations
+        # used for asserting all fields are replicated
+        stream_to_all_catalog_fields = dict()
+        for catalog in test_catalogs_all_fields:
+            stream_id, stream_name = catalog['stream_id'], catalog['stream_name']
+            catalog_entry = menagerie.get_annotated_schema(conn_id, stream_id)
+            fields_from_field_level_md = [md_entry['breadcrumb'][1]
+                                          for md_entry in catalog_entry['metadata']
+                                          if md_entry['breadcrumb'] != []]
+            stream_to_all_catalog_fields[stream_name] = set(
+                fields_from_field_level_md)
+        
+        # run initial sync
+        record_count_by_stream = self.run_and_verify_sync(conn_id)
+        synced_records = runner.get_records_from_target_output()
+
+        # Verify no unexpected streams were replicated
+        synced_stream_names = set(synced_records.keys())
+        self.assertSetEqual(expected_streams, synced_stream_names)
+        
+        
+        for stream in expected_streams:
+            with self.subTest(stream=stream):
+
+                # expected values
+                expected_all_keys = stream_to_all_catalog_fields[stream]
+                expected_automatic_keys = expected_automatic_fields.get(
+                    stream, set())
+
+                # Verify that you get some records for each stream
+                self.assertGreater(record_count_by_stream.get(stream, -1), 0)
+
+                # verify all fields for a stream were replicated
+                self.assertGreater(len(expected_all_keys), len(expected_automatic_keys))
+                self.assertTrue(expected_automatic_keys.issubset(
+                    expected_all_keys), msg='{} is not in "expected_all_keys"'.format(expected_automatic_keys-expected_all_keys))
+
+                messages = synced_records.get(stream)
+                # collect actual values
+                actual_all_keys = set()
+                for message in messages['messages']:
+                    if message['action'] == 'upsert':
+                        actual_all_keys.update(message['data'].keys())
+
+                self.assertSetEqual(expected_all_keys, actual_all_keys)
---
+++ tests/test_recharge_bookmark.py
@@ -0,0 +1,164 @@
+from tap_tester import menagerie, connections, runner
+from base import RechargeBaseTest
+
+
+class RechargeBookmarkTest(RechargeBaseTest):
+    """Recharge bookmarks test"""
+
+    def name(self):
+        return "tap_tester_recharge_bookmark_test"   
+    
+    def test_run(self):
+
+        """
+        Verify that:
+            For each stream you can do a sync which records bookmarks.
+            The bookmark is the maximum value sent to the target for the replication key.
+            A second sync respects the bookmark
+            All data of the second sync is >= the bookmark from the first sync
+            The number of records in the 2nd sync is less then the first (This assumes that
+                    new data added to the stream is done at a rate slow enough that you haven't
+                    doubled the amount of data from the start date to the first sync between
+                    the first sync and second sync run in this test)
+            For full table stream, all data replicated in sync 1 is replicated again in sync 2.
+        
+        PREREQUISITE
+        For EACH stream that is incrementally replicated there are multiple rows of data with
+            different values for the replication key
+        """
+
+        expected_streams = self.expected_streams()
+        expected_replication_keys = self.expected_replication_keys()
+        expected_replication_methods = self.expected_replication_method()
+
+        ################### First Sync #####################
+
+        conn_id = connections.ensure_connection(self)
+
+        # Run in check mode
+        found_catalogs = self.run_and_verify_check_mode(conn_id)
+
+        # table and field selection
+        catalog_entries = [
+            catalog
+            for catalog in found_catalogs
+            if catalog.get("tap_stream_id") in expected_streams
+        ]
+
+        self.perform_and_verify_table_and_field_selection(conn_id, catalog_entries)
+
+        # Run a first sync job using orchestrator
+        first_sync_record_count = self.run_and_verify_sync(conn_id)
+        first_sync_records = runner.get_records_from_target_output()
+        first_sync_bookmarks = menagerie.get_state(conn_id)
+
+        ######################## Update State Between Syncs ################################
+        new_states = {"bookmarks": dict()}
+        simulated_states = self.calculated_states_by_stream(first_sync_bookmarks)
+        for stream, new_state in simulated_states.items():
+            new_states["bookmarks"][stream] = new_state
+
+        menagerie.set_state(conn_id, new_states)
+
+        ##################### Second Sync #######################################
+        second_sync_record_count = self.run_and_verify_sync(conn_id)
+        second_sync_records = runner.get_records_from_target_output()
+        second_sync_bookmarks = menagerie.get_state(conn_id)
+
+        ##################### Test By Stream #########################
+
+        for stream in expected_streams:
+            with self.subTest(stream=stream):
+
+                # expected values
+                expected_replication_method = expected_replication_methods[stream]
+
+                # collect information for assertions from syncs 1 & 2 base on expected values
+                first_sync_count = first_sync_record_count.get(stream, 0)
+                second_sync_count = second_sync_record_count.get(stream, 0)
+                first_sync_messages = [
+                    record.get("data")
+                    for record in first_sync_records.get(stream, {}).get("messages", [])
+                    if record.get("action") == "upsert"
+                ]
+                second_sync_messages = [
+                    record.get("data")
+                    for record in second_sync_records.get(stream, {}).get("messages", [])
+                    if record.get("action") == "upsert"
+                ]
+                first_bookmark_value = first_sync_bookmarks.get("bookmarks", {stream: None}).get(stream)
+                second_bookmark_value = second_sync_bookmarks.get("bookmarks", {stream: None}).get(stream)
+
+                if expected_replication_method == self.INCREMENTAL:
+
+                    replication_key = list(expected_replication_keys[stream])[0]
+
+                    first_bookmark_value_ts = self.dt_to_ts(first_bookmark_value, self.DATETIME_FMT)
+
+                    second_bookmark_value_ts = self.dt_to_ts(second_bookmark_value, self.DATETIME_FMT)
+
+                    simulated_bookmark_value = self.dt_to_ts(new_states["bookmarks"][stream], self.DATETIME_FMT)
+
+                    # Verify the first sync sets a bookmark of the expected form
+                    self.assertIsNotNone(first_bookmark_value)
+                    
+                    # Verify the second sync sets a bookmark of the expected form
+                    self.assertIsNotNone(second_bookmark_value)
+
+                    # Verify the second sync bookmark is Equal to the first sync bookmark
+                    self.assertEqual(second_bookmark_value, first_bookmark_value)
+
+                    for record in first_sync_messages:
+
+                        # Verify the first sync bookmark value is the max replication key value for a given stream
+                        replication_key_value = self.dt_to_ts(record.get(replication_key), self.DATETIME_FMT)
+
+                        self.assertLessEqual(
+                            replication_key_value,
+                            first_bookmark_value_ts,
+                            msg="First sync bookmark was set incorrectly, a record with a greater replication-key value was synced.",
+                        )
+
+                    for record in second_sync_messages:
+                        # Verify the second sync replication key value is Greater or Equal to the first sync bookmark
+                        replication_key_value = self.dt_to_ts(record.get(replication_key), self.DATETIME_FMT)
+                        self.assertGreaterEqual(
+                            replication_key_value,
+                            simulated_bookmark_value,
+                            msg="Second sync records do not respect the previous bookmark.",
+                        )
+
+                        # Verify the second sync bookmark value is the max replication key value for a given stream
+                        self.assertLessEqual(
+                            replication_key_value,
+                            second_bookmark_value_ts,
+                            msg="Second sync bookmark was set incorrectly, a record with a greater replication-key value was synced.",
+                        )
+
+                    # verify that you get less data the 2nd time around
+                    self.assertLess(
+                        second_sync_count,
+                        first_sync_count,
+                        msg="second sync didn't have less records, bookmark usage not verified",
+                    )
+
+                elif expected_replication_method == self.FULL_TABLE:
+
+                    # Verify the syncs do not set a bookmark for full table streams
+                    self.assertIsNone(first_bookmark_value)
+                    self.assertIsNone(second_bookmark_value)
+
+                    # Verify the number of records in the second sync is the same as the first
+                    self.assertEqual(second_sync_count, first_sync_count)
+
+                else:
+
+                    raise NotImplementedError(
+                        "INVALID EXPECTATIONS\t\tSTREAM: {} REPLICATION_METHOD: {}".format(stream, expected_replication_method))
+
+                # Verify at least 1 record was replicated in the second sync
+                self.assertGreater(
+                    second_sync_count,
+                    0,
+                    msg="We are not fully testing bookmarking for {}".format(stream),
+                )
