8ff774362b29e4457e139c878b4c2e932b9ff571
Rutuja Deshmukh <107538720+rdeshmukh15@users.noreply.github.com>
2025-01-20 18:46:20 +0530

    fix dependabot issue and pylint fix (#44)

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-recharge',
-      version='2.0.3',
+      version='2.0.4',
       description='Singer.io tap for extracting data from the ReCharge Payments API 2.0',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_recharge'],
       install_requires=[
           'backoff==1.8.0',
-          'requests==2.31.0',
+          'requests==2.32.3',
           'singer-python==5.10.0'
       ],
       entry_points='''
